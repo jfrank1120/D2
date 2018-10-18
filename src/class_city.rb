87914ffce3c9f31372f75387347249780fa721e9
ryan.guild <ryan.guild@pyrantix.org>
2018-10-18 14:14:31 -0400

    look at all this work i did

--- src/class_city.rb
+++ src/class_city.rb
@@ -1,43 +1,42 @@
-#city datatype definition
+# city datatype definition
+class City
+  def initialize(rand, name, gold, silver)
+    @random = rand
+    @name = name
+    @gold = gold
+    @silver = silver
+    @links = []
+  end
 
-class City 
-    def initialize(rand, name, gold, silver)
-        @random = rand
-        @name = name
-        @gold = gold
-        @silver = silver
-        @links = []
-    end
+  def next_city?
+    @links[(@random.rand * @links.count - 1).floor]
+  end
 
-    def nextCity?
-        @links[(@random.rand * @links.count - 1).floor]
-    end
+  def add_link(link)
+    @links << link
+  end
 
-    def Addlink(link)
-        @links << link
-    end
+  def links?
+    @links
+  end
 
-    def links?
-        @links
-    end
+  def name?
+    @name
+  end
 
-    def name?
-        @name
-    end
+  def gold?
+    @gold
+  end
 
-    def gold?
-        @gold
-    end
+  def silver?
+    @silver
+  end
 
-    def silver? 
-        @silver
-    end
+  def mine_gold
+    @random.rand * @gold
+  end
 
-    def mine_gold
-        @random.rand * @gold
-    end
-
-    def mine_silver
-        @random.rand * @silver
-    end
+  def mine_silver
+    @random.rand * @silver
+  end
 end
--- src/class_prospector.rb
+++ src/class_prospector.rb
@@ -1,1 +1,57 @@
-# Prospector file
+# Prospector Object (Mutable)
+class Prospector
+  def initialize(map)
+    @gold_holding = 0
+    @silver_holding = 0
+    @curr_location = map.start?
+    @gold_found = 0
+    @silver_found = 0
+    @location_count = 0
+    @visited = []
+  end
+
+  def add_gold(amount)
+    @gold_holding += amount
+  end
+
+  def add_silver(amount)
+    @silver_holding += amount
+  end
+
+  def location?
+    @curr_location
+  end
+
+  def held_gold?
+    @gold_holding
+  end
+
+  def found_gold?
+    @gold_found
+  end
+
+  def held_silver?
+    @silver_holding
+  end
+
+  def found_silver?
+    @silver_found
+  end
+
+  def num_moves?
+    @location_count
+  end
+
+  def move(city)
+    unless @visited.include?(city)
+      @visited << city
+      @location_count += 1
+    end
+    @curr_location = city
+  end
+
+  def print_location
+    puts "The Prospector is now Aproaching: #{@curr_location.name?}"
+  end
+
+end
