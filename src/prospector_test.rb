9d6e8da0ad70ce036b69e0c16195bd69779d51ad
Jared Frank <jfrank1120@verizon.net>
2018-10-21 18:04:18 -0400

    Update 10/21
    -Created Prospector_test class
    -Created setup method within prospector test class

---
+++ src/prospector_test.rb
@@ -0,0 +1,17 @@
+
+
+require 'minitest/autorun'
+require_relative 'class_prospector'
+require_relative 'class_city'
+
+class ProstpectorTest < Minitest::Test
+
+    def setup
+        @prospect = Prospector::new
+    end
+
+    def test_prospector_is_prospector
+        
+    end
+
+    def test_
