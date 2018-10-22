6b379a12357a45a5f29b60821f5587cddf3c657f
Jared Frank <jfrank1120@verizon.net>
2018-10-21 21:37:53 -0400

    Update 10/21
    -Created new testing methods for both city and prospector class

--- src/prospector_test.rb
+++ src/prospector_test.rb
@@ -8,10 +8,19 @@ class ProstpectorTest < Minitest::Test
 
     def setup
         @prospect = Prospector::new
+        @city = City::new
     end
 
-    def test_prospector_is_prospector
-        
+    def test_prospector_is_not_nil
+        refute_nil @prospect
+        assert_kind_of Prospector, prospect
     end
 
-    def test_
+    def test_city_is_not_nil
+        refute_nil @city
+        assert_kind_of city, City
+    end
+
+    def test_mine_gold
+        assert_
+    end
