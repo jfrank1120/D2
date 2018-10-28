aaadc5e38940ad8551881fed43abc1dddff879e5
Jared Frank <jfrank1120@verizon.net>
2018-10-28 19:25:17 -0400

    Update 10/24 V.2
    -Created links between all cities
    -Began work on main loop for program

--- src/gold_rush.rb
+++ src/gold_rush.rb
@@ -3,8 +3,10 @@
 require_relative 'class_prospector'
 require_relative 'class_city'
 
+# Getting user seed passed in through command arguments
 userSeed = ARGV[2]
 
+# City object creation
 sutter_creek = city.new(userSeed, "Sutter Creek", 2, 0)
 coloma = city.new(userSeed, "Coloma", 3, 0)
 angels_camp = city.new(userSeed, "Angels Camp", 4, 0)
@@ -13,4 +15,31 @@ virginia_city = city.new(userSeed, "Virginia City", 3, 3)
 midas = city.new(userSeed, "Midas", 0, 5)
 el_dorado_cn = city.new(userSeed, "El Dorado Canyon", 0, 10)
 
-sutter_creek.add_link()
+# Sutter creek link creation
+sutter_creek.add_link(coloma)
+sutter_creek.add_link(angels_camp)
+
+# Coloma link creation
+coloma.add_link(virginia_city)
+coloma.add_link(sutter_creek)
+
+# Angels Camp link creation
+angels_camp.add_link(nevada_city)
+angels_camp.add_link(virginia_city)
+angels_camp.add_link(sutter_creek)
+
+# Nevada City link creation
+nevada_city.add_link(angels_camp)
+
+# Midas link creation
+midas.add_link(el_dorado_cn)
+midas.add_link(virginia_city)
+
+# El Dorado Canyon link creation
+el_dorado_cn.add_link(midas)
+el_dorado_cn.add_link(virginia_city)
+
+num_prospectors = ARGV[3]
+
+for j in 1..num_prospectors do
+    
