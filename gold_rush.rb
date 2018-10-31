9cacce9e02ccb779fa5a91b58e841c679acd8633
Jared Frank <jfrank1120@verizon.net>
2018-10-31 13:36:57 -0400

    RuboCop Offenses Update
    -Fixed Rubocop offenses
    -Current offense # = 4

--- gold_rush.rb
+++ gold_rush.rb
@@ -8,6 +8,16 @@ def check_args?(argv)
     argv[1].to_i.is_a?(Integer)
 end
 
+def game_loop(prospector, currmap)
+  while prospector.num_moves? <= 5
+    currmap.print_map(prospector.location?)
+    mine_metals(prospector)
+    prospector.move(prospector.location?.next_city?)
+    prospector.print_location
+  end
+  finish_trip(prospector)
+end
+
 def start_game(args)
   curr_num = 1
   args[1].to_i.times do
@@ -23,46 +33,49 @@ def mine_metals(prospector)
   p_num = prospector.number?.to_s
   # Mine for gold and silver in current city
   prospector.set_found_gold(prospector.location?.mine_gold)
-  print_findings('Gold', prospector.found_gold?.to_s, p_num)
+  print_findings('Gold', prospector.found_gold?, p_num)
   prospector.add_gold(prospector.found_gold?)
   prospector.set_found_silver(prospector.location?.mine_silver)
-  print_findings('Silver', prospector.found_silver?.to_s, p_num)
+  print_findings('Silver', prospector.found_silver?, p_num)
   prospector.add_silver(prospector.found_silver?)
 end
 
-def print_findings(metal_type, amt, p_num)
-  if amount > 0
-    puts '  Prospector ' + p_num + ' found ' + amt + ' Ounces of ' + metal_type
+def print_findings(metal_type, amt, num)
+  if amt.zero?
+    return
   end
+
+  amt_str = amt.to_s
+  puts '  Prospector ' + num + ' found ' + amt_str + ' Ounces of ' + metal_type
 end
 
 def finish_trip(prospector)
+  days_traveled_str = prospector.days_traveled?.to_s
   gold_val = prospector.held_gold? * 20.67
   silver_val = prospector.held_silver? * 1.31
-  puts 'After ' + prospector.days_traveled?.to_s + ' Days, Prospector ' + prospector.number?.to_s + ' returned to San Fransico with:'
+  puts 'After ' + days_traveled_str + ' Days, Prospector '
+  puts prospector.number?.to_s + ' returned to San Fransico with:'
+  print_trip_data(prospector, gold_val, silver_val)
+end
+
+def print_trip_data(prospector, gold_val, silver_val)
+  gold_str = gold_val.to_s
+  silver_str = silver_val.to_s
+  held_silver_str = prospector.held_silver?.to_s
+  held_gold_str = prospector.held_gold?.to_s
   if prospector.held_gold? != 1
-    puts prospector.held_gold?.to_s + " Ounces of Gold worth: $" + gold_val.to_s
+    puts held_gold_str + ' Ounces of Gold worth: $' + gold_str
   else
-    puts prospector.held_gold?.to_s + " Ounce of Gold worth: $" + gold_val.to_s
+    puts held_gold_str + ' Ounce of Gold worth: $ ' + gold_str
   end
   if prospector.held_silver? != 1
-    puts prospector.held_silver?.to_s + " Ounces of Silver worth: $" + silver_val.to_s
+    puts held_silver_str + ' Ounces of Silver worth: $' + silver_str
   else
-    puts prospector.held_silver?.to_s + " Ounce of Silver worth: $" + silver_val.to_s
+    puts held_silver_str + ' Ounce of Silver worth: $' + silver_str
   end
 end
 
 if check_args? ARGV
   args = ARGV
   start_game args
-end
-
-def game_loop(prospector, currmap)
-    while prospector.num_moves? <= 5
-        map.print_map(prospector.location?)
-        mine_metals(prospector)
-        prospector.move(prospector.location?.next_city?)
-        prospector.print_location
-    end
-    finish_trip(prospector)
 end
